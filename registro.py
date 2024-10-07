467835f5cc418a222f772a8cb5c0929b280e0e91
ghpancardo <ghpancardo@gmail.com>
2024-10-06 23:46:40 -0600

    Registro corregido para crear número secreto

--- registro.py
+++ registro.py
@@ -1,39 +1,57 @@
 import sqlite3
 import generarNumero
 
-def entradas():
+entradas = []
+
+def recibirEntradas():
+    global entradas
+    
     print("------------------------------------------------")
     print("")
     print("REGISTRO")
-    print ("Crear un nuevo usuario")
+    print("Crear un nuevo usuario")
     print("")
     print("------------------------------------------------")
-    usuario=input("Ingrese un nombre de usuario: ")
-    password=input("Ingrese una contraseña de 10 o más carácteres: ")
-    if (len(password)<10):
-        error=True
-        while (error==True):
-            print("Hay un problema con la longitud de su contraseña.")
-            password=input("Ingrese una contraseña de 10 o más carácteres: ")
-            if (len(password)>=10):
-                error=False
-    entradas=[usuario, password]
-    return entradas
+    
+    usuario = input("Ingrese un nombre de usuario: ")
+    password = input("Ingrese una contraseña de 10 o más caracteres: ")
+    
+    while len(password) < 10:
+        print("Hay un problema con la longitud de su contraseña.")
+        password = input("Ingrese una contraseña de 10 o más caracteres: ")
+    
+    entradas = [usuario, password]
 
 def escribirBase():
-    entradasUso=entradas()
+    recibirEntradas()
+    
     conexion = sqlite3.connect('usuarios.db')
     cursor = conexion.cursor()
+    
     cursor.execute('''
-INSERT INTO Usuario (nombre,  password) 
-VALUES (?, ?)
-''', (entradasUso[0], entradasUso[1]))
+    CREATE TABLE IF NOT EXISTS Usuario (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nombre TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+    ''')
+    
+    cursor.execute('''
+    INSERT INTO Usuario (nombre, password) 
+    VALUES (?, ?)
+    ''', (entradas[0], entradas[1]))
+    
     conexion.commit()
+    
     conexion.close()
+    
     print("Se ha creado un nuevo usuario")
     print("Recuerda cuidar tu contraseña.")
 
 def main():
     escribirBase()
     print("Se ha creado un archivo numero.secreto que es necesario para iniciar sesión.")
-    generarNumero()
+    generarNumero.numeroSecreto(entradas[1])
+
+if __name__ == "__main__":
+    main()
